b41d81e39571516de1a09a2d97c296f30a51d458
King Doggo <77363964+notahacker8@users.noreply.github.com>
2022-07-18 15:42:02 -0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,4 @@
 var script = document.createElement('script');
 script.src = "https://raw.githubusercontent.com/notahacker8/Browser-Request-Logger/main/code.js";
 document.head.appendChild(script);
+alert("bootstrapped!");
