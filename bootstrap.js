9e54781b2b55178cc3448114d728bb65179de86e
King Doggo <77363964+notahacker8@users.noreply.github.com>
2022-07-18 15:46:34 -0700

    Delete bootstrap.js

--- bootstrap.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.src = "https://raw.githubusercontent.com/notahacker8/Browser-Request-Logger/main/code.js";
-document.head.appendChild(script);
-alert("bootstrapped!");
